fa90b633950292af829001a3108705b1ce52d47e
Alexandre Shiroma <aleshiroma@gmail.com>
2025-11-28 17:25:41 -0300

    edge-functions test

--- frontend_vue/netlify/edge-functions/test.ts
+++ frontend_vue/netlify/edge-functions/test.ts
@@ -5,7 +5,7 @@ export const config: Config = { path: '/hello' };
 
 export default async (req: Request, ctx: Context) => {
   const client = new SuiClient({ url: getFullnodeUrl('devnet') });
-  const obj = await client.getObject({ id: '0x76d2dab98844227b531d73d9a873138b1a34b31cd347a8993d880e72dd6b3d28'});
+  const obj = await client.getObject({ id: '0x76d2dab98844227b531d73d9a873138b1a34b31cd347a8993d880e72dd6b3d28', options: { showContent: true }});
 
   return new Response(
     JSON.stringify({ ok: true, content: obj.data?.content}),
