dd9c8ea6d278ded87863493e022b0493decdadce
Alexandre Shiroma <aleshiroma@gmail.com>
2025-11-28 17:24:15 -0300

    +edge-functions

--- frontend_vue/netlify/edge-functions/test.ts
+++ frontend_vue/netlify/edge-functions/test.ts
@@ -1,9 +1,16 @@
+import type { Config, Context } from '@netlify/edge-functions';
 import { SuiClient, getFullnodeUrl } from '@mysten/sui/client';
 
-export const config = { path: '/hello' };
+export const config: Config = { path: '/hello' };
 
-export default async () => {
+export default async (req: Request, ctx: Context) => {
   const client = new SuiClient({ url: getFullnodeUrl('devnet') });
   const obj = await client.getObject({ id: '0x76d2dab98844227b531d73d9a873138b1a34b31cd347a8993d880e72dd6b3d28'});
-  return new Response(JSON.stringify(obj.data?.content), { headers: { 'Content-Type': 'application/json' } });
+
+  return new Response(
+    JSON.stringify({ ok: true, content: obj.data?.content}),
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  );
 };
